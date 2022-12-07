281e3ffd820930e8195819bf1c0dc2d99cc7c4db
Kazimir Kovalenko <mirkovalenko@gmail.com>
2022-12-07 21:52:17 +0100

    7 completed

--- 7/7.py
+++ 7/7.py
@@ -19,7 +19,7 @@ class Directory:
         self.content: list[File] = []
         self.child_directories: dict[str, 'Directory'] = {}
         self.file_size: int = 0
-        self._total_size: int = 0
+        self._size: int = 0
 
     def add_file(self, file: File):
         self.content.append(file)
@@ -36,19 +36,17 @@ class Directory:
                 yield directory
 
     @property
-    def total_size(self):
-        if self._total_size:
-            return self._total_size
+    def size(self):
+        if self._size:
+            return self._size
         else:
-            self._total_size = self.file_size
+            self._size = self.file_size
             for directory in self.child_directories.values():
-                self._total_size += directory.total_size
-            return self._total_size
+                self._size += directory.size
+            return self._size
 
 
-directories: dict[str, Directory] = dict()
-matched_directories_total_size = 0
-root_directory = None
+root_directory = Directory(name="/")
 
 with open(input_file, "r") as file:
     for line in file:
@@ -58,10 +56,8 @@ with open(input_file, "r") as file:
             if line_items[1] == "cd":
                 dir_name = line_items[2]
                 if dir_name == "/":
-                    root_directory = current_directory = Directory(name=dir_name)
+                    current_directory = root_directory
                 elif dir_name == "..":
-                    if current_directory.total_size <= 100000:
-                        matched_directories_total_size += current_directory.total_size
                     current_directory = current_directory.parent
                 else:
                     child_directory = Directory(name=dir_name, parent=current_directory)
@@ -74,18 +70,19 @@ with open(input_file, "r") as file:
 
 
 matched_directories_size = 0
-root_size = root_directory.total_size
+root_subdirectories_flat = list(root_directory.get_all_subdirectories())
 
-for subdir in root_directory.get_all_subdirectories():
-    if subdir.total_size <= 100000:
-        matched_directories_total_size += subdir.total_size
+for subdir in root_subdirectories_flat:
+    if subdir.size <= 100000:
+        matched_directories_size += subdir.size
 
-print("Part 1 answer: ", matched_directories_total_size)
+print("Part 1 answer: ", matched_directories_size)
 
+root_size = root_directory.size
+required_space = 30000000
+need_to_free_up_space = required_space - (70000000 - root_size)
 
-# required_space = 30000000
-# need_to_free_up_space = required_space - (70000000 - root_size)
-
-# for subdir in sorted(subdirectories_flat, key=lambda s: s.total_size):
-#     if subdir.total_size >= need_to_free_up_space:
-#         print("Part 2 answer: ", subdir.name)
+for subdir in sorted(root_subdirectories_flat, key=lambda s: s.size):
+    if subdir.size >= need_to_free_up_space:
+        print("Part 2 answer: ", subdir.size)
+        break
