fd7a7339e1ab962caa59d6caaa790e22e0e09335
Kazimir Kovalenko <mirkovalenko@gmail.com>
2022-12-07 21:38:48 +0100

    7_2

--- 7/7_1.py
+++ 7/7_1.py
@@ -28,6 +28,13 @@ class Directory:
     def add_directory(self, directory: 'Directory'):
         self.child_directories[directory.name] = directory
 
+    def get_all_subdirectories(self):
+        for directory in self.child_directories.values():
+            yield directory
+            
+            for directory in directory.get_all_subdirectories():
+                yield directory
+
     @property
     def total_size(self):
         if self._total_size:
@@ -37,21 +44,11 @@ class Directory:
             for directory in self.child_directories.values():
                 self._total_size += directory.total_size
             return self._total_size
-s = set()
-l = set()
-c = {'ljv', 'wqcsrw', 'jllhmmf'}
 
 
 directories: dict[str, Directory] = dict()
 matched_directories_total_size = 0
 root_directory = None
-current_path = []
-
-
-def construct_path(path: list[int]) -> str:
-    p = "/".join(path)
-    return "/".join(path)
-
 
 with open(input_file, "r") as file:
     for line in file:
@@ -65,25 +62,30 @@ with open(input_file, "r") as file:
                 elif dir_name == "..":
                     if current_directory.total_size <= 100000:
                         matched_directories_total_size += current_directory.total_size
-                        if current_directory.name in c:
-
-                            print("match", construct_path(current_path), current_directory.name)
-                    else:
-                        if current_directory.name in c:
-                            print("dismatch", construct_path(current_path), current_directory.name)
                     current_directory = current_directory.parent
-                    current_path.pop()
                 else:
-                    current_path.append(current_directory.name)
-                    current_directory = current_directory.child_directories[dir_name]
+                    child_directory = Directory(name=dir_name, parent=current_directory)
+                    current_directory.add_directory(child_directory)
+                    current_directory = child_directory
         else:
-            if line_items[0] == "dir":
-                child_directory = Directory(name=line_items[1], parent=current_directory)
-                current_directory.add_directory(child_directory)
-            else:
+            if line_items[0] != "dir":
                 current_file = File(name=line_items[1], size=int(line_items[0]))
                 current_directory.add_file(current_file)
-    if root_directory.total_size <= 100000:
-        matched_directories_total_size += root_directory.total_size
 
-print(matched_directories_total_size)
+
+matched_directories_size = 0
+root_size = root_directory.total_size
+
+for subdir in root_directory.get_all_subdirectories():
+    if subdir.total_size <= 100000:
+        matched_directories_total_size += subdir.total_size
+
+print("Part 1 answer: ", matched_directories_total_size)
+
+
+# required_space = 30000000
+# need_to_free_up_space = required_space - (70000000 - root_size)
+
+# for subdir in sorted(subdirectories_flat, key=lambda s: s.total_size):
+#     if subdir.total_size >= need_to_free_up_space:
+#         print("Part 2 answer: ", subdir.name)
