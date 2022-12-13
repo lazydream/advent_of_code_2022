b82e49bf83bd9940f3acbf443ce1eddb04529333
Kazimir Kovalenko <kazimir.kovalenko@swisscom.com>
2022-12-13 17:16:12 +0100

    12

--- 12/12.py
+++ 12/12.py
@@ -7,40 +7,67 @@ input_file = os.path.join(script_path, "input.txt")
 height_map = {c: i for i in ascii_lowercase}
 height_map["S"] = height_map["a"]
 height_map["E"] = height_map["z"]
+graph_map = dict()
 
-grid = []
-visited = set()
-start_position = None
-end_position = None
-with open(input_file, "r") as file:
-    for i, line in enumerate(file):
-        line = line.strip()
-        row = []
-        for j, c in enumerate(line):
-            row.append(c)
-            if c == "S":
-                start_position = (i, j)
-            if c == "E":
-                end_position = (i, j)
-        grid.append(row)
 
-distance_map = {
-    start_position: 0
-}
+class Graph:
+    def __init__(self, val: str, key: tuple[int, int]) -> None:
+        self.val = val
+        self.key = key
+        self.adjacent = dict()
+    
+    def add_neighbour(self, graph: 'Graph') -> None:
+        self.adjacent[graph.key] = graph
 
-def _find_possible_moves(i: int, j: int) -> list[tuple[int, int]]:
+    def has_neighbour(self, key: tuple[int, int]) -> bool:
+        return key in self.adjacent
+
+
+def _get_connected_graphs(i: int, j: int) -> list[Graph]:
     cur_height = height_map[grid[i][j]]
-    possible_moves = []
+    connected_graphs = []
     for ni, nj in ((i+1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
         if ni >= 0 and nj >= 0:
             if cur_height >= height_map[grid[ni][nj]] - 1:
-                possible_moves.append((ni, nj))
+                graph = _get_or_create_graph(ni, nj)
+                connected_graphs.append(graph)
 
-def djkstra_req(i: int, j: int) -> None:
-    cur = grid[i, j]
-    available_neighbours = _find_possible_moves(i, j)
-    for (ni, nj) in available_neighbours:
-        if (ni, nj) not in visited:
-            cur_distance = distance_map[i, j] + 1
-            distance_map[ni, nj] = cur_distance
-            visited.add(ni, nj)
+
+def _get_or_create_graph(i, j) -> Graph:
+    if (i, j) not in graph_map:
+        graph = Graph(c, (i, j))
+        graph_map[(i, j)] = graph
+    else:
+        graph = graph_map[(i, j)]
+    return graph
+
+
+grid = []
+visited = set()
+start_graph = None
+# end_position = None
+with open(input_file, "r") as file:
+    for i, line in enumerate(file):
+        line = line.strip()
+        for j, c in enumerate(line):
+            graph = _get_or_create_graph(i, j)
+            for connected_graph in _get_connected_graphs(i, j):
+                if not graph.has_neighbour(connected_graph.key):
+                    graph.add_neighbour(graph)
+                else:
+                    print("check is needed")
+            if graph.val == "S":
+                start_graph = graph
+
+
+distance_map = {
+    start_graph.key: 0
+}
+
+
+def djkstra_req(graph: Graph) -> None:
+    for connected_graph in graph.adjacent.values():
+        if connected_graph.key not in visited:
+            cur_distance = distance_map[graph.key] + 1
+            distance_map[connected_graph.key] = cur_distance
+    visited.add(graph.key)
